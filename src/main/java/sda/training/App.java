4a80ea9b697790842e91dd344515259a3adc561f
Adam Pusio <adam.pusio@outlook.com>
2022-03-23 19:10:49 +0100

    7-stworzenie-glownego-menu - podstawowe menu bez petli

--- src/main/java/sda/training/App.java
+++ src/main/java/sda/training/App.java
@@ -4,6 +4,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import sda.training.learning.Car;
 
+import java.util.Scanner;
+
 public class App
 {
     public static void main( String[] args )
@@ -14,11 +16,40 @@ public class App
         SessionFactory sessionFactory = hibernateFactory.getSessionFactory();
         Session session = sessionFactory.openSession();
 
-        Car car = new Car();
-        car.setName("maluch");
-        session.save(car);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("WYBIERZ GRE: ");
+        System.out.println("1. Tu wstaw gre 🚀🚀🚀");
+        System.out.println("2. Tu wstaw gre 🚀🚀🚀");
+        System.out.println("3. Tu wstaw gre 🚀🚀🚀");
+        System.out.println("4. Tu wstaw gre 🚀🚀🚀");
+        int gameNumber = scanner.nextInt();
+
+        switch (gameNumber){
+            case 1:
+                System.out.println("Zaimplementuj mnie 🤣");
+                break;
+            case 2:
+                System.out.println("Zaimplementuj mnie 🤣");
+                break;
+            case 3:
+                System.out.println("Zaimplementuj mnie 🤣");
+                break;
+            case 4:
+                System.out.println("Zaimplementuj mnie 🤣");
+                break;
+            default:
+                System.out.println("Nie mam takiej gry 😒");
+        }
+
 
         session.close();
         sessionFactory.close();
+    }
+
+    private static Car storingCarInDb(Session session) {
+        Car car = new Car();
+        car.setName("maluch");
+        session.save(car);
+        return car;
     }
 }
